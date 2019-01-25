d41d663b7d6411b1f1c679c35369f89a813dfbe6
jan bos <max.bos217@gmail.com>
2019-01-25 11:58:06 +0100

    FontChaos name

--- Fontchaos/Form1.Designer.cs
+++ Fontchaos/Form1.Designer.cs
@@ -28,13 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ChooseFileTextBox = new System.Windows.Forms.TextBox();
+            this.ChooseFileButton = new System.Windows.Forms.Button();
+            this.RandomizeButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // ChooseFileTextBox
+            // 
+            this.ChooseFileTextBox.Location = new System.Drawing.Point(115, 12);
+            this.ChooseFileTextBox.Name = "ChooseFileTextBox";
+            this.ChooseFileTextBox.Size = new System.Drawing.Size(217, 22);
+            this.ChooseFileTextBox.TabIndex = 0;
+            // 
+            // ChooseFileButton
+            // 
+            this.ChooseFileButton.Location = new System.Drawing.Point(12, 11);
+            this.ChooseFileButton.Name = "ChooseFileButton";
+            this.ChooseFileButton.Size = new System.Drawing.Size(97, 23);
+            this.ChooseFileButton.TabIndex = 1;
+            this.ChooseFileButton.Text = "Choose file";
+            this.ChooseFileButton.UseVisualStyleBackColor = true;
+            this.ChooseFileButton.Click += new System.EventHandler(this.GetFileClick);
+            // 
+            // RandomizeButton
+            // 
+            this.RandomizeButton.Location = new System.Drawing.Point(12, 40);
+            this.RandomizeButton.Name = "RandomizeButton";
+            this.RandomizeButton.Size = new System.Drawing.Size(320, 36);
+            this.RandomizeButton.TabIndex = 2;
+            this.RandomizeButton.Text = "Randomize";
+            this.RandomizeButton.UseVisualStyleBackColor = true;
+            this.RandomizeButton.Click += new System.EventHandler(this.RandomizeButtonClick);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 99);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(673, 554);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(697, 665);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.RandomizeButton);
+            this.Controls.Add(this.ChooseFileButton);
+            this.Controls.Add(this.ChooseFileTextBox);
+            this.Name = "Form1";
+            this.Text = "FontChaos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox ChooseFileTextBox;
+        private System.Windows.Forms.Button ChooseFileButton;
+        private System.Windows.Forms.Button RandomizeButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- Fontchaos/Form1.cs
+++ Fontchaos/Form1.cs
@@ -3,18 +3,69 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Office;
+
+
 
 namespace Fontchaos
 {
     public partial class Form1 : Form
     {
+
+
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        OpenFileDialog FileChecked;
+        bool HasFile = false;
+
+        private void GetFileClick(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new
+                   System.IO.StreamReader(openFileDialog1.FileName);
+                sr.Close();
+                CheckFile(openFileDialog1);
+            }
+        }
+
+        private void CheckFile(OpenFileDialog TXTFile) {
+            if (Path.GetExtension(TXTFile.FileName) == ".txt")
+            {
+                ChooseFileTextBox.Text = TXTFile.FileName;
+                FileChecked = TXTFile;
+                HasFile = true;
+                richTextBox1.Text = File.ReadAllText(FileChecked.FileName);
+            }
+        }
+
+        private void RandomizeButtonClick(object sender, EventArgs e) { 
+            Random rnd = new Random();
+            for (int i = 0; i < richTextBox1.TextLength; i++)
+            {
+                richTextBox1.Select(i, 1);
+
+                richTextBox1.SelectionFont = new Font(FontFamily.Families[rnd.Next(FontFamily.Families.Count())], 12f + rnd.Next(6));
+                richTextBox1.SelectionColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+                richTextBox1.SelectionBackColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+            }
+            
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
